7d753eb19198604ce901bb9834f91078d77df380
pqkkkk <22120174@student.hcmus.edu.vn>
2025-09-06 15:02:56 +0700

    feat: update CORS configuration to use frontend URL from properties and add frontend URL configuration in application-gcp.yaml

--- src/main/java/org/pqkkkkk/my_day_server/config/CorsConfig.java
+++ src/main/java/org/pqkkkkk/my_day_server/config/CorsConfig.java
@@ -2,6 +2,7 @@ package org.pqkkkkk.my_day_server.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,10 +11,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
-     @Bean
+    @Value("${frontend.url}")
+    private String frontendUrl;
+    
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedOriginPatterns(List.of(frontendUrl));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type","application/json"));
         corsConfiguration.setAllowCredentials(true);
